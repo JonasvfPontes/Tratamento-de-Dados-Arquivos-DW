0711e70dc57117756e47a754e021f79fac9dafa3
Jonas Pontes <jonasvfpontes@gmail.com>
2023-06-15 13:58:25 -0300

    Adição a opção de remover item ao ignotar TABs

--- scripts/config.py
+++ scripts/config.py
@@ -7,16 +7,17 @@ def fIgnorarTabs():
         O que deseja fazer agora?
         1 - Ver lista atual
         2 - Adicionar item à lista
-        3 - Sair
+        3 - Excluir item da lista
+        4 - Sair
         ''')
         if opc.upper() == "VOLTAR":
             break
         if opc =='1':
             print(dfLista)
-        elif opc == '3':
-            break
+        
         elif opc =='2':
-            print('Digite "Fim" a qualquer momento:')
+            print('Digite "Fim" a qualquer momento')
+            print('='*50)
             novosArquivos = []            
             while True:
                 arquivo = input('Nome do Arquivo [Ex. DW000 ou 000]: ')
@@ -33,7 +34,24 @@ def fIgnorarTabs():
                     dfLista.to_csv(r'scripts\\ignorarTabs.csv', index=False)
                     break
                 novosArquivos.append('DW' + arquivo)
-                
+        elif opc == '3':
+            print('Digite "Fim" a qualquer momento')
+            print('='*50)
+            while True:
+                i = input('Digite o número da linha que deseja excluir: ')
+                if i.upper() == 'FIM':
+                    
+                    dfLista.to_csv(r'scripts\\ignorarTabs.csv', index=False)
+                    break
+                elif i.isnumeric():
+                    dfLista = dfLista.drop(index=int(i))
+                    print('Feito')
+                else:
+                    print('Não entendi esse valor, tente novamente')
+
+        
+        elif opc == '4':
+            break
 
 
 def fIgnorar():
